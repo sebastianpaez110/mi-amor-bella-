5ac2754073fc8d488a88e826f25b85b2aa124e7b
Sebastian Paez <paezjonnatan89@gmail.com>
2026-08-03 22:33:30 -0500

    Corregir galería

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-let galeria=document.getElementById("galeria");
+let galeria = document.getElementById("galeria");
 
-for(let i=1;i<=90;i++){
-
-let foto=document.createElement("img");
-
-foto.src="imagenes/foto"+i+".jpg";
-
-galeria.appendChild(foto);
-
+for (let i = 1; i <= 90; i++) {
+    let foto = document.createElement("img");
+    foto.src = imagenes/jpeg (${i}).jpeg;
+    galeria.appendChild(foto);
 }
